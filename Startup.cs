52af2beb50928f45ce64c7862e3060979f11a9d6
caleb <caleb.sachaj@tek-experts.com>
2021-07-09 16:48:04 -0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[assembly: OwinStartup(typeof(WebApplication4.Startup))]
+
+namespace WebApplication4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
